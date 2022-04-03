5fb3e94500599cf331ed6f9808c2e7f59cbffb6e
sudeepsubedi <sudeep.subedi@ufl.edu>
2022-04-03 15:39:01 -0400

    Implemented data binding. Added buildFeature to build.graddle(module), layout to activity_main.xml and used the binding feature in MainActivity.kt

--- app/src/main/java/com/example/timetableapp/MainActivity.kt
+++ app/src/main/java/com/example/timetableapp/MainActivity.kt
@@ -5,31 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.example.timetableapp.databinding.ActivityMainBinding
 
 //import com.example.timetableapp.MyClass as MyClass1
 
 class MainActivity : AppCompatActivity()
 {
+    //private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     var classList:MutableList<MyClass> = mutableListOf<MyClass>()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         addClasses()
         printClassInfo()
 
         //When a button is clicked: These steps happen
         //Step 1: Get reference to the button
-        val btnMon1 = findViewById<Button>(R.id.mon1)
+        val btnMon1 = binding.mon1
         //Step 2: Call the clickHandlerFunction
-        btnMon1.setOnClickListener { clickHandlerFunction(it) }
+        btnMon1.setOnClickListener { clickHandlerFunction(btnMon1) }
     }
 
     //This clickHandler handles the onClick functionality for the buttons
-    private fun clickHandlerFunction(viewThatIsClicked: View)
+    private fun clickHandlerFunction(btn: Button)
     {
-        val btn = findViewById<Button>(R.id.mon1)
+        //val btn = findViewById<Button>(R.id.mon1)
         btn.text = classList[0].csciName  + "\n" +classList[0].classTime
         val size = btn.textSize
         btn.setTextSize(size-53)
